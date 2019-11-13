839a76f60bb7a51f26c543aeb318e882c45304a6
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-11-12 21:32:26 -0500

    Add files via upload

--- cww10.js
+++ cww10.js
@@ -1,5 +1,19 @@
-//Given an integral number, determine if it's a square number
+//You need count how many valleys you will pass.
 
-var isSquare = function(num1){
-    return num1 >= 0 && Math.sqrt(num1) % 1 === 0; 
-   }
+// Start is always from zero level.
+
+// Every time you go down below 0 level counts as an entry of a valley, and as you go up to 0 level from valley counts as an exit of a valley.
+
+// One passed valley is equal one entry and one exit of a valley.
+
+function valleys(v) {
+    //here we go again
+    let level=0;
+    let valleys = 0;
+    v.split("").forEach(el=>{
+      if(level===-1&&el==="U") valleys++;
+      if(el==="D") level-=1;
+      if(el==="U") level+=1;
+    })
+    return valleys;
+  }
