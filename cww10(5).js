3a73918056559b8b26aad29992855f22f0c8a88c
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-12-06 00:34:39 -0500

    Update cww10(5).js

--- cww10(5).js
+++ cww10(5).js
@@ -1,36 +1,11 @@
-// You're going on a trip with some students and it's up to you to keep track of how much money each Student has. A student is defined like this:
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
 
-// class Student {
-//   constructor(name, fives, tens, twenties) {
-//     this.name = name;
-//     this.fives = fives;
-//     this.tens = tens;
-//     this.twenties = twenties;
-//   }
-// }
-// As you can tell, each Student has some fives, tens, and twenties. Your job is to return the name of the student with the most money. If every student has the same amount, then return "all".
-
-// Notes:
-
-// Each student will have a unique name
-// There will always be a clear winner: either one person has the most, or everyone has the same amount
-// If there is only one student, then that student has the most money
-function mostMon (students){
-let array = [];
-if (students.length === 1) {
-   return students[0].name;
-   console.log('students***', students[0].name);
+function cookie(x){
+    if (typeof(x) === 'string'){
+    return "Who ate the last cookie? It was Zach!"
+    }else if (typeof (x) === 'number' || typeof(x) === 'float'){
+    return "Who ate the last cookie? It was Monica!"
+    }else {
+    return "Who ate the last cookie? It was the dog!"
 }
-students.forEach((value, index) => {
-   let total = ((5 * value.fives) + (10 * value.tens) + (20 * value.twenties));
-   array.push([total, value.name]);
-});
-array = array.sort((a, b) => b[0] - a[0]);
-console.log('array****', array);
-if (array.every((el, i, array) => el[0] === array[0][0])) {
-  return 'all'; 
-}
-else {
-  return array[0][1];
-}
-}
+};
