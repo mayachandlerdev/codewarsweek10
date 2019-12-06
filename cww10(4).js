7c7882bfd8eb3dd9c1ebfc94a7b35fe7038bc099
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-12-06 00:31:45 -0500

    Update cww10(4).js

--- cww10(4).js
+++ cww10(4).js
@@ -1,14 +1,19 @@
-//code wars 
-//At each stage an alternating sequence of 1s and 0s is inserted between the terms of the previous sequence.
-
-// Define a generator function paperFold that sequentially generates the values of this sequence.
-
-function* paperFold() {
-    yield 1;
-    const gen = paperFold();
-    while ( true )
-      yield gen.next().value,
-      yield 0,
-      yield gen.next().value,
-      yield 1;
-  }
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(number) {
+let type = number;
+let a, b, temp = 0;
+b = type;
+while(type > 0){
+    a = type % 10;
+    type = parseInt(type/10)
+    temp = temp * 10 + a
+}
+    if(temp === b){
+        return true
+    }else{
+        return false
+    }
+};
