b708e8f031fd5ea5d842717a912d85453c427389
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-12-06 00:30:31 -0500

    Update codewars4-6.js

--- codewars4-6.js
+++ codewars4-6.js
@@ -1,20 +1,7 @@
-// You are given an input string.
-
-// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
-
-function numericals(input) {
-    let obj = {};
-    let result = "";
-    for (let i = 0; i < input.length; i += 1) {
-        let current = input[i];
-        if (obj[current]) {
-            obj[current] += 1;
-        } else {
-            obj[current] = 1;
-        }
-
-        result += obj[current];
-    }
-
-    return result;
-}
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+ return nums.reduce((a, b) => a ^ b, 0)
+};
